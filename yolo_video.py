0a4bee716b653216a3ad50351ed814b2b147dc9f
Akeem Wells <awells@humite.local>
2021-10-26 21:20:21 -0400

    Minor Tweaks to Image Viewing

--- yolo_video.py
+++ yolo_video.py
@@ -4,18 +4,21 @@ from yolo import YOLO, detect_video
 from PIL import Image
 
 def detect_img(yolo):
-    while True:
-        img = input('Input image filename:')
-        try:
-            image = Image.open(img)
-            r_image = yolo.detect_image(image)
-            r_image.show()
-        except:
-            print('Open Error! Try again!')
-            continue
-        else:
-            r_image = yolo.detect_image(image)
-            r_image.show()
+    #while True:
+    img = input('Input image filename:')
+    image = Image.open(img)
+    r_image = yolo.detect_image(image)
+    r_image.show()
+        #try:
+        #    image = Image.open(img)
+        #    r_image = yolo.detect_image(image)
+        #    r_image.show()
+        #except:
+        #    print('Open Error! Try again!')
+        #    continue
+        #else:
+        #    r_image = yolo.detect_image(image)
+        #    r_image.show()
     yolo.close_session()
 
 FLAGS = None
